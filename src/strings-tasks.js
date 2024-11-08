7e8f9ee37c0f3126ec9e1f7719ce767ef37169c9
Нина Евлаш <evlash.nina@mail.ru>
2024-11-08 20:09:47 +0100

    add task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -258,7 +258,9 @@ function endsWith(str, substr) {
  *   formatTime(0, 0) => "00:00"
  */
 function formatTime(minutes, seconds) {
-  
+  const min = minutes.toString().padStart(2, '0');
+  const sec = seconds.toString().padStart(2, '0');
+  return `${min}:${sec}`;
 }
 
 /**
